6e424fda9817282003faa2a838f7dbabe3a7b4a0
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 20:59:14 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d8372db555             |
+    |             670d85fda49ca             |
     |_______________________________________|
 */
  use Pmpr\Module\Jalali\Jalali; Jalali::symcgieuakksimmu();
--- src/Pmpr/Module/Jalali/Asset.php
+++ src/Pmpr/Module/Jalali/Asset.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d8372db555             |
+    |             670d85fda49ca             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Jalali; use Pmpr\Common\Foundation\Interfaces\Constants; class Asset extends Container { }
