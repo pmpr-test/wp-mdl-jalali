7b870fdbec9b96f52b8dd34480411c2b8be4e869
pmpr.dev <pmpr.dev@gmail.com>
2025-11-30 22:03:17 +0000

    Obfuscated

--- src/Pmpr/Module/Jalali/Plugin/Dokan/Dokan.php
+++ src/Pmpr/Module/Jalali/Plugin/Dokan/Dokan.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             692c4d420abbd             |
+    |             692cbef715131             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Jalali\Plugin\Dokan; use Pmpr\Module\Jalali\Container; class Dokan extends Container { public function mameiwsayuyquoeq() { Checkout::symcgieuakksimmu(); SellerDashboard::symcgieuakksimmu(); } }
