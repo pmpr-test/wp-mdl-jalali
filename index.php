6a842d219693e45477ba5f94188b88706f24bcbb
pmpr.dev <pmpr.dev@gmail.com>
2025-12-03 12:26:41 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             693022928abce             |
+    |             69302c52d1084             |
     |_______________________________________|
 */
  use Pmpr\Module\Jalali\Jalali; Jalali::symcgieuakksimmu();
